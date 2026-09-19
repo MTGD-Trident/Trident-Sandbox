4404eae2d458a7ecefc323c4925be15d41187594
Trident Build <trident-build@local>
2026-09-19 05:18:14 +0000

    Initial Capacitor project for Trident sandbox Android build

---
+++ android/app/src/main/java/com/trident/sandbox/MainActivity.java
@@ -0,0 +1,5 @@
+package com.trident.sandbox;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.trident.sandbox',
+  appName: 'Trident Sandbox',
+  webDir: 'www'
+};
+
+export default config;
